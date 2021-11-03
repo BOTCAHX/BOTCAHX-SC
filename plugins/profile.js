f756bd14cbf79707e0becea720aa74e4e19f0334
RTeam1 <77674437+RTeam1@users.noreply.github.com>
2021-11-03 20:50:22 +0700

    Add files via upload

--- plugins/profile.js
+++ plugins/profile.js
@@ -15,7 +15,7 @@ let handler = async (m, { conn, usedPrefix }) => {
         limit: 10,
         lastclaim: 0,
         registered: false,
-        name: conn.getName(m.sender),
+        name: 'Belum Daftar:v',
         age: -1,
         regTime: -1,
         afk: -1,
@@ -34,13 +34,14 @@ let handler = async (m, { conn, usedPrefix }) => {
     let math = max - xp
     let res = `http://hardianto-chan.herokuapp.com/api/rankcard?profile=https://i.ibb.co/vQTHzkh/IMG-20210907-WA0721.jpg&name=${name}&bg=https://i.ibb.co/4YBNyvP/images-76.jpg&needxp=${max}&curxp=${exp}&level=${level}&logorank=https://i.ibb.co/Wn9cvnv/FABLED.png`
     let str = `
-Nama: ${username} ${registered ? '(' + name + ') ' : ''}(@${who.replace(/@.+/, '')})${about != 401 ? '\nInfo: ' + about : ''}
+Nama: ${registered ? '(' + name + ') ' : ''}(@${who.replace(/@.+/, '')})${about != 401 ? '\nInfo: ' + about : ''}
 Nomor: ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
 Link: https://wa.me/${who.split`@`[0]}${registered ? '\nUmur: ' + age : ''}
 XP: TOTAL ${exp} (${exp - min} / ${xp}) [${math <= 0 ? `Siap untuk *${usedPrefix}levelup*` : `${math} XP lagi untuk levelup`}]
 Level: ${level}
 Role: *${role}*
 Limit: ${limit}
+Owner: ${global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender) ? 'Ya' : 'Tidak'}
 Premium: ${global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender) ? 'Ya' : 'Tidak'}
 Terdaftar: ${registered ? 'Ya (' + new Date(regTime).toLocaleString() + ')' : 'Tidak'}${lastclaim > 0 ? '\nTerakhir Klaim: ' + new Date(lastclaim).toLocaleString() : ''}
 `.trim()
