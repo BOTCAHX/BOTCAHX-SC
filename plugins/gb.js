92088063db28417dd1d70ee6047c9076644ff919
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-05 16:41:14 +0700

    Update gb.js

--- plugins/gb.js
+++ plugins/gb.js
@@ -2,7 +2,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 ye = `@${m.sender.split`@`[0]}`
 let info = `Hai Kak ${ye} Nih SC VIA GITHUB ${ye} JANGAN LUPA FOLLOW
 Atau Bisa Lewat Link Dibawah Ini :
-https://github.com/RTeam1
+
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', `*${global.packname}*`, 'status@broadcast') 
