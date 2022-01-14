9c70771dc7e75ec6b4f49f3f0685fe5538c9a88e
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-15 04:15:33 +0700

    Update addprem.js

--- plugins/addprem.js
+++ plugins/addprem.js
@@ -1,18 +1,22 @@
-const { MessageType } = require('@adiwajshing/baileys')
+let fs = require('fs')
+let handler = async (m, { conn, text }) => {
 
-let handler = async(m, { conn, text }) => {
-let who
-  if (m.isGroup) who = m.mentionedJid[0]
-    else who = m.chat
-      if (!who) throw 'Tag salah satu lah,dan masukkan nomor untuk di verivikasi !'
-        // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
-            global.DATABASE._data.chats[m.chat].premium = true
-              var nomor = m.sender
-                  m.reply(`*Done berhasil added User✅*\n\n*Nomor : wa.me/${nomor.split("@s.whatsapp.net")[0]}\n*Expired:* 30Days\n*Thanks For Added Premium !*`)
-                    // } else m.reply('Ada nomor host disini...')
-                    }
-                    handler.help = ['addprems <nomor>']
-                    handler.tags = ['owner']
-                    handler.command = /^addprems$/i
-                    handler.rowner = true
+    const json = JSON.parse(fs.readFileSync('./src/premium.json'))
+        let who
+            if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '') + '@s.whatsapp.net'
+                else who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
+                    if (json.includes(who.split`@`[0])) throw `@${who.split`@`[0]} sudah premium!`
+                        json.push(`${who.split`@`[0]}`)
+                            fs.writeFileSync('./src/premium.json', JSON.stringify(json))
+                                m.reply(`@${who.split`@`[0]} sekarang premium!`)
+                                //@${m.who.split`@`[0]} ////@${who.split`@`[0]}
+                                    delete require.cache[require.resolve('../config')]
+                                        require('../config')
+}
+handler.help = ['addprem [@user]']
+handler.tags = ['owner']
+handler.command = /^(add|tambah|\+)prem$/i
+
+handler.owner = true
+
 module.exports = handler
