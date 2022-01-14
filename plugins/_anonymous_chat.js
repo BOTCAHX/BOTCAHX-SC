83653e73643e82bb01acffbd5d173be83cc5cf5c
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-15 06:21:23 +0700

    Update _anonymous_chat.js

--- plugins/_anonymous_chat.js
+++ plugins/_anonymous_chat.js
@@ -1,36 +1,23 @@
-module.exports = {
-name: ["start"], 
-type: ['anonymous chat'],
-description: "anonymous chat seperti telegram",
-utilisation: global.userbot.prefix+ "start",
+let handler = m => m
 
-async execute(m) {
-//snsn
-if (Object.values(this.anonymous).find(room => room.check(m.sender))) {
-                await this.sendButton(m.chat, 'Kamu masih berada di dalam anonymous chat, menunggu partner...\n\nKetik ${userbot.prefix}leave untuk berhenti\natau menekan button dibawah ini', userbot.author, 'LEAVE', `${userbot.prefix}leave`, m)
-                throw false
-            }
-            let room = Object.values(this.anonymous).find(room => room.state === 'WAITING' && !room.check(m.sender))
-            if (room) {
-                await this.send2Button(room.a, 'Partner ditemukan!\n\n${userbot.prefix}next : Untuk skip\n${userbot.prefix}leave : Untuk berhenti\n\natau kamu dapat menekan button dibawah', userbot.author, 'NEXT', `${userbot.prefix}next`, 'LEAVE', `${userbot.prefix}leave`, m)
-                room.b = m.sender
-                room.state = 'CHATTING'
-                await this.send2Button(room.a, 'Partner ditemukan!\n\n${userbot.prefix}next : Untuk skip\n${userbot.prefix}leave : Untuk berhenti\n\natau kamu dapat menekan button dibawah', userbot.author, 'NEXT', `${userbot.prefix}next`, 'LEAVE', `${userbot.prefix}leave`, m)
-            } else {
-                let id = + new Date
-                this.anonymous[id] = {
-                    id,
-                    a: m.sender,
-                    b: '',
-                    state: 'WAITING',
-                    check: function (who = '') {
-                        return [this.a, this.b].includes(who)
-                    },
-                    other: function (who = '') {
-                        return who === this.a ? this.b : who === this.b ? this.a : ''
-                    },
-                }
-                await this.sendButton(m.chat, 'Menunggu partner...\n\nKetik ${userbot.prefix}leave untuk berhenti\natau menekan button dibawah ini', userbot.author, 'LEAVE', `${userbot.prefix}leave`, m)
-            }
-        }
-    }
+handler.before = async function (m, { match }) {
+    // if (match) return !1
+        if (!m.chat.endsWith('@s.whatsapp.net')) return !0
+            this.anonymous = this.anonymous ? this.anonymous : {}
+                let room = Object.values(this.anonymous).find(room => [room.a, room.b].includes(m.sender) && room.state === 'CHATTING')
+                    if (room) {
+                            if (/^.*(next|leave|start)/.test(m.text)) return
+                                    if (['.next', '.leave', '.start', 'Cari Partner', 'Keluar', 'Next'].includes(m.text)) return
+                                            let other = [room.a, room.b].find(user => user !== m.sender)
+                                                    m.copyNForward(other, true, m.quoted && m.quoted.fromMe ? {
+                                                                contextInfo: {
+                                                                                ...m.msg.contextInfo,
+                                                                                                forwardingScore: 1,
+                                                                                                                isForwarded: true,
+                                                                                                                                participant: other
+                                                                                                                                            }
+                                                                                                                                                    } : {})
+                                                                                                                                                        }
+                                                                                                                                                            return !0
+                                                                                                                                                            }
+module.exports = handler
