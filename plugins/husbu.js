f4e890c33e32040769ab48b5a534bfab38574b82
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-17 16:38:00 +0700

    Update husbu.js

--- plugins/husbu.js
+++ plugins/husbu.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler  = async (m, { conn, usedPrefix, command }) => {
 m.reply(wait)
-heum = await fetch(`https://api.lolhuman.xyz/api/random/husbu?apikey=${lolkey}`)
+heum = await fetch(`https://api.xteam.xyz/randomimage/husbu?APIKEY=MahliKey`)
 json = await heum.buffer()
 // conn.sendFile(m.chat, json, 'loli.jpg', 'Lomlinya kak', m, false)
 conn.sendButtonImg(m.chat, json, kasihcaption, footer, 'NEXT', `${usedPrefix + command}`, m)
