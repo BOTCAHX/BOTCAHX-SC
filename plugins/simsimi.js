0bf4054b1acdba911ea365f18f5f4b7a7c299560
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-17 17:25:01 +0700

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,12 +1,14 @@
 let fetch = require('node-fetch')
-let handler = async (m, { text }) => {
-  let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "en" }, ''))
+let handler = async (m, { text, usedPrefix, command }) => {
+  if (!text) throw `uhm.. teksnya mana?\n\ncontoh:\n${usedPrefix + command} hai`
+  let res = await fetch(global.API('pencarikode', '/api/simsimii', { text: encodeURIComponent(text) }, 'apikey'))
+  if (!res.ok) throw eror
   let json = await res.json()
-  if (json.success) m.reply(json.success)
-  else throw json
+  if (json.result == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.') await m.reply('siminya blom diajarin jadi gatau t_t custom pesannya di https://simsimi.com/teach')
+  await m.reply(`*Simi:* ${json.result}`)
 }
-handler.help = ['bot'].map(v => v + ' <teks>')
+handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
 handler.tags = ['fun']
-handler.command = /^(bot|simi)$/i
+handler.command = /^((sim)?simi|simih)$/i
 
 module.exports = handler
