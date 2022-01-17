56222979df0496b88275bf902fb97821b10642d6
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-18 03:32:47 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -34,9 +34,7 @@ const defaultMenu = {
 ${'```%npmdesc```'}
 `,
 }
-let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
-	let bzz = fs.readFileSync('')
-	let bzz2 = fs.readFileSync('')
+
 	let { anon, anticall, antispam, antitroli, backup, jadibot, groupOnly, nsfw } = global.db.data.settings[conn.user.jid]
     let totaljadibot = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
 
