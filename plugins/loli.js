6461adc48ee4810d8bf71043a26400e24c24f90c
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-17 16:26:25 +0700

    Update loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/lolice', {
     avatar: await conn.getProfilePicture(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-  }), 'lolice.png', 'liuliuliuliuliu we heard that there is a lolicon here', m)
+  }), 'lolice.png', 'case by wa.me/6282221792667', m)
 }
 
 handler.help = ['loli']
