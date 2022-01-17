6480f9775addb59fcbaea593a8c157226cefce37
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-18 03:51:44 +0700

    Update addprem.js

--- plugins/addprem.js
+++ plugins/addprem.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
     else who = m.chat
     if (!who) throw `tag the person!`
-    if (global.prems.includes(who.split`@`[0])) throw 'Dia sudah premium!'
+    if (global.prems.includes(who.split`@`[0])) throw 'dia sudah premium!'
     global.prems.push(`${who.split`@`[0]}`)
     conn.reply(m.chat, `@${who.split`@`[0]} Sekarang premium!`, m, {
         contextInfo: {
