2288c03edcfea8c233682d8e1af9a6363a6ff701
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-18 02:53:49 +0700

    Update lloli.js

--- plugins/lloli.js
+++ plugins/lloli.js
@@ -1,24 +1,13 @@
-let fetch = require('node-fetch')
-     let handler  = async (m, { conn, usedPrefix, command }) => {
-     	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
-m.reply(wait)
-heum = await fetch(`https://api.lolhuman.xyz/api/random/nsfw/loli?apikey=${lolkey}`)
-    json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, kasihcaption, footer, 'Next', `${usedPrefix + command}`, m, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+let Booru = require('booru')
 
+let handler = async(m, { conn }) => {
+m.reply('Loading...')
+let img = await Booru.search('kn', ['loli'], { random: true })
+img = img[0].data
+await conn.sendFile(m.chat, img.file_url, '', '', m)
 }
-handler.help = ['nsfwloli']
-handler.tags = ['hentai']
-handler.command = /^nsfwloli$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
+handler.help = ['loli']
+handler.tags = ['anime']
+handler.command = /^(loli)$/i
 
 module.exports = handler
