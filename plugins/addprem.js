8c67b984820dd74fb370a5371c0cf6fd580fa577
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-18 03:03:04 +0700

    Update addprem.js

--- plugins/addprem.js
+++ plugins/addprem.js
@@ -2,16 +2,17 @@ let fs = require('fs')
 let handler = async (m, { conn, text }) => {
 
     const json = JSON.parse(fs.readFileSync('./src/premium.json'))
-        let who
-            if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '') + '@s.whatsapp.net'
-                else who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
-                    if (json.includes(who.split`@`[0])) throw `@${who.split`@`[0]} sudah premium!`
-                        json.push(`${who.split`@`[0]}`)
-                            fs.writeFileSync('./src/premium.json', JSON.stringify(json))
-                                m.reply(`@${who.split`@`[0]} sekarang premium!`)
-                                //@${m.who.split`@`[0]} ////@${who.split`@`[0]}
-                                    delete require.cache[require.resolve('../config')]
-                                        require('../config')
+    let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '') + '@s.whatsapp.net'
+    else who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
+    if (json.includes(who.split`@`[0])) throw `${conn.getName(who)} sudah premium!`
+    json.push(`${who.split`@`[0]}`)
+    fs.writeFileSync('./src/premium.json', JSON.stringify(json))
+    m.reply(`${conn.getName(who)} sekarang premium!`)
+
+    delete require.cache[require.resolve('../config')]
+    require('../config')
+
 }
 handler.help = ['addprem [@user]']
 handler.tags = ['owner']
