0349714b8c87642e2b1c1c9b800466157efecd22
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-23 08:33:48 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -240,7 +240,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
           "listType": "SINGLE_SELECT",
           "sections": [
             {
-	    "title": `${ucapan()}, ${name}`,
+	    "title": `${ucapan()}, Pilih Menu Nya`,
               "rows": [
                 {
                   "title": `[🧾] Semua Perintah`,
