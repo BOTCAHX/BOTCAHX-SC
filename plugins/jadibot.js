937cffddd637a80662cb40727d826a4947fa6dd9
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-25 20:03:25 +0700

    Update jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -70,8 +70,8 @@ handler.help = ['jadibot']
 handler.tags = ['jadibot', 'premium']
 
 handler.command = /^jadibot$/i
-handler.premium = false
+handler.premium = true
 handler.private = true
-handler.limit = 10000
+handler.limit = 5
 
 module.exports = handler
