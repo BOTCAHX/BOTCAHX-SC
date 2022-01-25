08190945dc998a0f3a308ea2f8a1248cbde92fd6
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-26 00:37:32 +0700

    Update akungratis.js

--- plugins/akungratis.js
+++ plugins/akungratis.js
@@ -1,174 +1,132 @@
 let handler = async m => m.reply(`
 「 AKUN GRATIS 」
 
-• M�I�N�E�C�R�A�F�T� �(�X�B�O�X�)�
+• MINECRAFT (XBOX)
 
-�1�.� �U�s�e�r�n�a�m�e� �:� �n�i�k�i�t�a�s�f�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �:� �t�a�s�f�e�l�1�3�
-�
-�2�.� �U�s�e�r�n�a�m�e� �:� �i�c�u�d�x�i�i�i�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �:� �S�p�e�n�c�e�r�1�3�
-�
-�3�.� �U�s�e�r�n�a�m�e� �:� �j�a�c�k�a�l�l�0�4�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �:� �F�o�x�t�a�i�l�1�9�
-�
-�4�.� �U�s�e�r�n�a�m�e� �:� �z�a�i�t�s�e�v�2�0�2�0�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �:� �s�n�i�p�e�r�m�4�n�
-�
-�5�.� �U�s�e�r�n�a�m�e� �:� �j�o�k�e�r�2�0�1�9�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �:� �l�e�g�e�n�d�v�e�r�s�i�o�n�2�0�0�7�
-�
-�6�.� �U�s�e�r�n�a�m�e� �:� �c�o�r�o�n�a�m�a�n�1�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �:� �v�i�r�u�s�b�o�o�s�t�e�d�1�
-�
-�7�.� �U�s�e�r�n�a�m�e� �:� �e�x�t�e�r�n�a�b�l�e�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �:� �r�e�m�o�t�e�1�2�
-�
-�8�.� �U�s�e�r�n�a�m�e� �:� �d�r�s�t�r�a�n�g�e�0�1�@�h�o�t�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �:� �3�5�6�9�8�5�1�4�2�6�
-�
-�9�.� �U�s�e�r�n�a�m�e� �:� �r�a�d�i�o�s�t�a�n�c�i�q�2�2�5�@�g�m�a�i�l�.�c�o�m�P�a�s�s�w�o�r�d� �:� �s�t�e�n�l�i�1�2�3�
-�
-�1�0�.� �U�s�e�r�n�a�m�e� �:� �m�a�j�o�r�k�i�n�g�5�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �:� �5�5�m�a�j�o�r�5�5�
-�
-• F�R�E�E� �F�I�R�E�
-�
-�1�.� �E�m�a�i�l� �:� �a�l�y�u�n�u�s�0�6�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �:� �@�0�6�0�7�1�9�9�0�2�2�
-�H�a�n�d�p�h�o�n�e� �:� �0�8�7�7�8�3�3�8�1�2�9�6�
-�
-�2�.� �E�m�a�i�l� �:� �m�u�k�u�l�v�e�r�m�a�4�9�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �:� �m�u�c�o�o�l�1�4�
-�H�a�n�d�p�h�o�n�e� �:� �+�9�1�9�8�0�2�8�1�1�4�1�4�
-�
-�3�.� �E�m�a�i�l� �:� �a�m�i�t�s�h�a�r�m�a�.�0�7�8�2�1�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �:� �A�n�j�e�e�t�a�_�s�h�a�r�m�a�
-�H�a�n�d�p�h�o�n�e� �:� �+�9�1�8�1�4�5�7�7�7�6�5�7�
-�
-�4�.� �E�m�a�i�l� �:� �s�y�a�h�r�u�l�m�u�t�a�f�i�f�i�n�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �:� �1�2�3�4�f�a�t�i�m�a�h�
-�H�a�n�d�p�h�o�n�e� �:� �0�8�9�6�2�6�7�0�2�8�4�8�
-�
-�5�.� �E�m�a�i�l� �:� �m�a�r�c�e�l�r�i�k�u�d�o�@�y�a�h�o�o�.�c�o�m�
-�P�a�s�s�w�o�r�d� �:� �m�a�r�c�e�l�0�1�
-�H�a�n�d�p�h�o�n�e� �:� �0�8�1�3�6�6�4�8�3�3�0�3�
-�
-�6�.� �E�m�a�i�l� �:� �m�a�d�d�y�_�3�3�3�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �:� �s�h�o�n�y�a�d�h�o�l�u�
-�H�a�n�d�p�h�o�n�e� �:� �0�9�8�2�2�2�5�9�1�1�1�
-�
-�7�.� �E�m�a�i�l� �:� �w�a�w�a�n�f�o�l�d�7�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �:� �I�n�d�r�a� �f�q�
-�H�a�n�d�p�h�o�n�e� �:� �+�6�2�8�2�2�5�1�1�2�2�6�0�8�
-�
-�8�.� �E�m�a�i�l� �:� �g�i�a�n�g�o�d�a�4�7�7�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �:� �P�a�s�s�w�o�r�d�
-�H�a�n�d�p�h�o�n�e� �:� �8�7�1�8�0�2�1�2�6�8�
-�
-�9�.� �E�m�a�i�l� �:� �t�s�h�a�r�m�a�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �:� �h�a�n�u�m�a�n�0�0�1�
-�H�a�n�d�p�h�o�n�e� �:� �9�8�9�1�4�9�7�1�1�4�
-�
-�1�0�.� �E�m�a�i�l� �:� �A�d�b�1�9�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �:� �1�2�4�3�5�6�a�b�h�i�
-�H�a�n�d�p�h�o�n�e� �:� �9�8�9�1�0�9�8�9�2�1�
-�
-• M�O�B�I�L�E� �L�E�G�E�N�D�S�
-�
-�1�.� �E�m�a�i�l� �G�o�o�g�l�e� �:� �e�j�a�k�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �G�o�o�g�l�e� �:� �1�2�3�4�5�6�7�
-�N�o�m�o�r� �H�p� �:� �0�8�2�2�8�1�0�8�0�5�1�2�
-�E�m�a�i�l� �F�a�c�e�b�o�o�k�:� �4�n�d�i�k�a�0�1�
-�P�a�s�s�w�o�r�d� �F�a�c�e�b�o�o�k� �:� �y�s�f�i�f�o�y�
-�
-�2�.� �E�m�a�i�l� �G�o�o�g�l�e� �:� �k�a�d�e�x�a�n�d�i�k�a�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �G�o�o�g�l�e� �:� �0�4�0�4�9�8�k�a�$�
-�N�o�m�o�r� �H�p� �:� �0�8�2�2�8�1�0�8�0�5�1�2�
-�E�m�a�i�l� �F�a�c�e�b�o�o�k�:� �4�n�d�i�k�a�0�1�
-�P�a�s�s�w�o�r�d� �F�a�c�e�b�o�o�k� �:� �0�4�0�4�9�8�k�a�$�
-�
-�3�.� �E�m�a�i�l� �G�o�o�g�l�e� �:� �a�r�a�k�o�s�w�a�r�a�3�3�0�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �G�o�o�g�l�e� �:� �1�2�3�t�a�s�i�k�1�2�3�
-�N�o�m�o�r� �H�p� �:� �0�8�5�6�0�3�2�6�6�1�2�1�
-�E�m�a�i�l� �F�a�c�e�b�o�o�k�:� �K�e�b�i�w�
-�P�a�s�s�w�o�r�d� �F�a�c�e�b�o�o�k� �:� �t�a�s�i�k�1�2�3�
-�
-�4�.� �E�m�a�i�l� �G�o�o�g�l�e� �:� �r�a�m�a�m�e�l�o�d�i�c�@�y�a�h�o�o�.�c�o�.�i�d�
-�P�a�s�s�w�o�r�d� �G�o�o�g�l�e� �:� �m�o�d�o�l�s�o�t�o�
-�N�o�m�o�r� �H�p� �:� �0�8�9�4�3�4�3�
-�E�m�a�i�l� �F�a�c�e�b�o�o�k�:� �r�a�m�a�d�h�a�n�
-�P�a�s�s�w�o�r�d� �F�a�c�e�b�o�o�k� �:� �m�o�d�o�l�s�o�t�o�
-�
-�5�.� �E�m�a�i�l� �G�o�o�g�l�e� �:� �a�r�i�f�z�a�i�n�u�d�i�n�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �G�o�o�g�l�e� �:� �a�r�i�f�1�2�3�4�5�6�
-�N�o�m�o�r� �H�p� �:� �0�8�1�2�2�2�8�8�9�8�7�7�
-�E�m�a�i�l� �F�a�c�e�b�o�o�k�:� �a�r�i�f�z�a�i�n�u�d�i�n�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �F�a�c�e�b�o�o�k� �:� �1�2�3�4�5�6�7�8�9�
-�
-�6�.� �E�m�a�i�l� �G�o�o�g�l�e� �:� �g�u�n�c�o�r�o�j�a�t�i�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �G�o�o�g�l�e� �:� �j�a�t�i�j�a�t�i�
-�N�o�m�o�r� �H�p� �:� �0�8�5�8�7�5�6�3�1�0�5�2�
-�E�m�a�i�l� �F�a�c�e�b�o�o�k�:� �0�8�5�6�4�7�8�1�8�8�6�3�
-�P�a�s�s�w�o�r�d� �F�a�c�e�b�o�o�k� �:� �p�a�e�r�h�o�s�o�k�y�e�s�s�
-�
-�7�.� �E�m�a�i�l� �G�o�o�g�l�e� �:� �d�e�f�i�d�e�f�l�e�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �G�o�o�g�l�e� �:� �0�1�j�u�l�i�1�9�9�4�
-�N�o�m�o�r� �H�p� �:� �0�8�5�2�8�0�3�2�7�9�5�5�
-�E�m�a�i�l� �F�a�c�e�b�o�o�k�:� �U�l�h�y�y�u�l�i�i�@�y�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �F�a�c�e�b�o�o�k� �:� �0�1�j�u�l�i�1�9�9�4�
-�
-�8�.� �E�m�a�i�l� �G�o�o�g�l�e� �:� �a�a�b�y�7�9�7�0�9�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �G�o�o�g�l�e� �:� �m�c�h�c� �m�a�l�a�n�g�
-�N�o�m�o�r� �H�p� �:� �0�8�2�2�4�1�8�4�8�8�4�5�
-�E�m�a�i�l� �F�a�c�e�b�o�o�k�:� �0�8�1�2�3�5�6�0�0�4�0�
-�P�a�s�s�w�o�r�d� �F�a�c�e�b�o�o�k� �:� �a�l�b�i�m�a�f�i�t�r�a�
-�
-�9�.� �E�m�a�i�l� �G�o�o�g�l�e� �:� �a�b�i�c�o�k�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �G�o�o�g�l�e� �:� �a�b�i�c�o�k�a�s�u�
-�N�o�m�o�r� �H�p� �:� �0�8�1�2�3�9�8�7�4�4�0�8�
-�E�m�a�i�l� �F�a�c�e�b�o�o�k�:� �A�b�i�a�s�u�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �F�a�c�e�b�o�o�k� �:� �a�b�i�a�s�u�c�o�k�
-�
-�1�0�.� �E�m�a�i�l� �G�o�o�g�l�e� �:� �j�c�x�d�a�m�h�a�@�g�m�a�i�l�.�c�o�m�
-�P�a�s�s�w�o�r�d� �G�o�o�g�l�e� �:� �d�a�m�h�a�1�2�3�
-�N�o�m�o�r� �H�p� �:� �0�8�5�9�3�9�1�2�2�2�4�
-�E�m�a�i�l� �F�a�c�e�b�o�o�k�:� �0�8�3�8�3�4�7�0�0�7�3�9�
-�P�a�s�s�w�o�r�d� �F�a�c�e�b�o�o�k� �:� �d�a�m�h�a�1�2�
-�
-• P�U�B�G�
-�
-�1�.� �E�m�a�i�l�:� �A�d�w�i�l�i�r�e�d�2�0�0�1�2�@�c�a�r�a�q�u�.�c�o�m�
-�K�a�t�a� �s�a�n�d�i�:� �t�e�r�m�i�n�a�n�t�o�r�2�9�
-�
-�2�.� �E�m�a�i�l�:� �B�r�e�r�a�c�i�a�@�g�m�a�i�l�.�c�o�m�
-�K�a�t�a� �s�a�n�d�i�:� �c�o�o�l�k�i�d�b�r�e�s�
-�
-�3�.� �E�m�a�i�l�:� �Y�b�a�l�i�w�y�t�h�F�A�V�@�g�m�a�i�l�.�c�o�m�
-�K�a�t�a� �s�a�n�d�i�:� �s�u�p�e�r�i�o�r�m�a�n�_�
-�
-�4�.� �E�m�a�i�l�:� �D�r�u�n�y�t�h�G�e�n�i�u�s�@�g�m�a�i�l�.�c�o�m�
-�K�a�t�a� �s�a�n�d�i�:� �1�2�3�4�5�6�8�2�
-�
-�5�.� �E�m�a�i�l�:� �D�r�u�n�y�t�h�P�U�B�G�2�0�2�@�g�m�a�i�l�.�c�o�m�
-�K�a�t�a� �s�a�n�d�i�:� �2�1�0�2�1�9�8�5�
-�
-�6�.� �E�m�a�i�l�:� �O�n�o�r�e�t�@�h�o�t�m�a�i�l�.�c�o�m�
-�K�a�t�a� �s�a�n�d�i�:� �o�n�o�g�e�n�i�z�t�t�a�1�2�
-�
-�7�.� �E�m�a�i�l�:� �H�e�l�i�d�i�a�n�e�w�a�c�c�@�g�m�a�i�l�.�c�o�m�
-�K�a�t�a� �s�a�n�d�i�:� �f�e�l�i�x�i�l�o�v�e�y�o�u�
-�
-�8�.� �E�m�a�i�l�:� �A�d�w�i�l�i�r�e�d�2�0�0�1�2�@�g�m�a�i�l�.�c�o�m�
-�K�a�t�a� �s�a�n�d�i�:� �t�e�r�m�i�n�a�n�t�o�r�2�9�
-�
-�9�.� �E�m�a�i�l�:� �B�r�e�r�a�c�i�a�@�g�m�a�i�l�.�c�o�m�
-�K�a�t�a� �s�a�n�d�i�:� �c�o�o�l�k�i�d�b�r�e�s�
-�
-�1�0�.� �E�m�a�i�l�:� �Y�b�a�l�i�w�y�t�h�F�A�V�@�g�m�a�i�l�.�c�o�m�
-�K�a�t�a� �s�a�n�d�i�:� �s�u�p�e�r�i�o�r�m�a�n�_�
+1. Username : nikitasf@gmail.com
+Password : tasfel13
+2. Username : icudxiii@gmail.com
+Password : Spencer13
+3. Username : jackall04@gmail.com
+Password : Foxtail19
+4. Username : zaitsev2020@gmail.com
+Password : sniperm4n
+5. Username : joker2019@gmail.com
+Password : legendversion2007
+6. Username : coronaman1@gmail.com
+Password : virusboosted1
+7. Username : externable@gmail.com
+Password : remote12
+8. Username : drstrange01@hotmail.com
+Password : 3569851426
+9. Username : radiostanciq225@gmail.comPassword : stenli123
+10. Username : majorking5@gmail.com
+Password : 55major55
+• FREE FIRE
+1. Email : alyunus06@gmail.com
+Password : @0607199022
+Handphone : 087783381296
+2. Email : mukulverma49@gmail.com
+Password : mucool14
+Handphone : +919802811414
+3. Email : amitsharma.07821@gmail.com
+Password : Anjeeta_sharma
+Handphone : +918145777657
+4. Email : syahrulmutafifin@gmail.com
+Password : 1234fatimah
+Handphone : 089626702848
+5. Email : marcelrikudo@yahoo.com
+Password : marcel01
+Handphone : 081366483303
+6. Email : maddy_333@gmail.com
+Password : shonyadholu
+Handphone : 09822259111
+7. Email : wawanfold7@gmail.com
+Password : Indra fq
+Handphone : +6282251122608
+8. Email : giangoda477@gmail.com
+Password : Password
+Handphone : 8718021268
+9. Email : tsharma@gmail.com
+Password : hanuman001
+Handphone : 9891497114
+10. Email : Adb19@gmail.com
+Password : 124356abhi
+Handphone : 9891098921
+• MOBILE LEGENDS
+1. Email Google : ejak@gmail.com
+Password Google : 1234567
+Nomor Hp : 082281080512
+Email Facebook: 4ndika01
+Password Facebook : ysfifoy
+2. Email Google : kadexandika@gmail.com
+Password Google : 040498ka$
+Nomor Hp : 082281080512
+Email Facebook: 4ndika01
+Password Facebook : 040498ka$
+3. Email Google : arakoswara330@gmail.com
+Password Google : 123tasik123
+Nomor Hp : 085603266121
+Email Facebook: Kebiw
+Password Facebook : tasik123
+4. Email Google : ramamelodic@yahoo.co.id
+Password Google : modolsoto
+Nomor Hp : 0894343
+Email Facebook: ramadhan
+Password Facebook : modolsoto
+5. Email Google : arifzainudin@gmail.com
+Password Google : arif123456
+Nomor Hp : 081222889877
+Email Facebook: arifzainudin@gmail.com
+Password Facebook : 123456789
+6. Email Google : guncorojati@gmail.com
+Password Google : jatijati
+Nomor Hp : 085875631052
+Email Facebook: 085647818863
+Password Facebook : paerhosokyess
+7. Email Google : defidefle@gmail.com
+Password Google : 01juli1994
+Nomor Hp : 085280327955
+Email Facebook: Ulhyyulii@ymail.com
+Password Facebook : 01juli1994
+8. Email Google : aaby79709@gmail.com
+Password Google : mchc malang
+Nomor Hp : 082241848845
+Email Facebook: 08123560040
+Password Facebook : albimafitra
+9. Email Google : abicok@gmail.com
+Password Google : abicokasu
+Nomor Hp : 081239874408
+Email Facebook: Abiasu@gmail.com
+Password Facebook : abiasucok
+10. Email Google : jcxdamha@gmail.com
+Password Google : damha123
+Nomor Hp : 08593912224
+Email Facebook: 083834700739
+Password Facebook : damha12
+• PUBG
+1. Email: Adwilired20012@caraqu.com
+Kata sandi: terminantor29
+2. Email: Breracia@gmail.com
+Kata sandi: coolkidbres
+3. Email: YbaliwythFAV@gmail.com
+Kata sandi: superiorman_
+4. Email: DrunythGenius@gmail.com
+Kata sandi: 12345682
+5. Email: DrunythPUBG202@gmail.com
+Kata sandi: 21021985
+6. Email: Onoret@hotmail.com
+Kata sandi: onogeniztta12
+7. Email: Helidianewacc@gmail.com
+Kata sandi: felixiloveyou
+8. Email: Adwilired20012@gmail.com
+Kata sandi: terminantor29
+9. Email: Breracia@gmail.com
+Kata sandi: coolkidbres
+10. Email: YbaliwythFAV@gmail.com
+Kata sandi: superiorman_
 
-「 BOTCAH 」
+「 MIKE BOT 」
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['akungratis','freeaccount']
 handler.tags = ['tools']
