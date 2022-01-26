ed9c10128d5bb77aa1a0b58a43beaa9ed9dfd29e
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-27 06:04:47 +0700

    Update viagopay.js

--- plugins/viagopay.js
+++ plugins/viagopay.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
 	conn.reply(m.chat, `╠═〘 PEMBAYARAN 〙 ═
 ╠═ Untuk Pembayaran Via Gopay
 ╠➥ Ke nomer ini
-║➥ Tidak menginstal
+║➥ Maaf Belum Tersedia Untuk Gopay
 ║
 ╠═〘 Fitur 〙 ═
 ║
