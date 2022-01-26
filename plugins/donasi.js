17398a0e2fff111e51edf146c48a7393fa83f4b0
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-27 06:06:59 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ PULSA : 082221792667
+├ PULSA : 081395861695
 ├ DANA : 082221792667
 └────
 `.trim(), footer, 'Owner', '.owner')
