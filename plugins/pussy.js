a2a5d7a8b2de0b8fd5d74e072248195bff0f79e9
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-30 05:20:59 +0700

    Update pussy.js

--- plugins/pussy.js
+++ plugins/pussy.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix, command }) => {
 	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
   m.reply('Sedang Diproses...')
-  bzz = await fetch(`https://api.xteam.xyz/randomimage/pussy?apikey=${xteamkey}`)
+  bzz = await fetch(`https://api.xteam.xyz/randomimage/pussy?apikey=apivproject`)
   res = await bzz.buffer()
   conn.sendButtonImg(m.chat, res, kasihcaption, footer, 'Next', `${usedPrefix + command}`, m, { contextInfo: { forwardingScore: 999, isForwarded: true }})
 
@@ -24,4 +24,3 @@ handler.fail = null
 handler.limit = true
 
 module.exports = handler
-
