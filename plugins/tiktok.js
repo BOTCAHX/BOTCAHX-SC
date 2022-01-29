ad98e0ef8133498121f008114a4d79e73cde3ae1
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-30 01:08:41 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -5,7 +5,7 @@ let handler = async(m, { conn, usedPrefix, text, command }) => {
     let res = await fetch(global.API('rey', '/api/download/tiktok', { url: text }, 'apikey'))
     if (!res.ok) throw await `${res.status} ${res.statusText}`
     let json = await res.json()
-    await conn.sendFile(m.chat, json.result.nowatermark, '', 'Nih kak', m)
+    await conn.sendFile(m.chat, json.result.nowatermark, '', '©BOTCAHX', m)
 }
 handler.command = /^tiktok$/i
 handler.tags = ['downloader']
