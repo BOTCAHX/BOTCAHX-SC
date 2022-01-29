87d7a59946e54529fa53d5f32921dd44f280dc89
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-30 05:00:56 +0700

    Update ppcouple.js

--- plugins/ppcouple.js
+++ plugins/ppcouple.js
@@ -1,15 +1,20 @@
 let fetch = require("node-fetch")
 
 let handler = async (m, { conn }) => {
-  let res = await fetch(global.API('LeysCoder', '/api/ppcouple', {}, 'apikey'))
-  if (!res.ok) throw await res.text()
-  let json = await res.json()
-  if (!json.status) throw json
-  await conn.sendFile(m.chat, json.result.male, '', 'case by wa.me/6282221792667', m)
-  await conn.sendFile(m.chat, json.result.female, '', 'case by wa.me/6282221792667', m)
+//https://api.xteam.xyz/randomimage/ppcouple?APIKEY=cristian9407
+m.reply(wait)
+
+let res = await fetch(`https://api.xteam.xyz/randomimage/ppcouple?APIKEY=cristian9407`)
+let json = await res.json()
+
+conn.sendFile(m.chat, json.result.male, 'ppcowo.png', 'Cowok', m, false,{ contextInfo: { forwardingScore: 999, isForwarded: true }})
+
+conn.sendFile(m.chat, json.result.female, 'ppcewe.png', 'Cewek', m,false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+
 }
-handler.help = ['ppcouple/ppcp']
-handler.tags = ['anime']
+handler.help = ['ppcouple', 'ppcp']
+handler.tags = ['internet', 'anime']
 handler.command = /^(pp(cp|couple))$/i
+handler.limit = true
 
 module.exports = handler
