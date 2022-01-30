c0e3eb1abcfa99d7a499cfe178408ac4d430b167
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-31 02:45:07 +0700

    Update githubdl.js

--- plugins/githubdl.js
+++ plugins/githubdl.js
@@ -1,18 +1,18 @@
-let handler = async (m, { args, usedprefix, command }) => {
+let handler = async (m, { args, usedPrefix, command }) => {
 
-if (!args[0]) throw 'usernamenya mana om\ncontoh ilmanhdyt'
-if (!args[1]) throw 'repo nya mana?\ncontoh shiraoribot'
+if (!args[0]) throw 'where\'s the username ?'
+if (!args[1]) throw 'where\'s the repo?'
 
-let url = `https://github.com/${args[0]}/${args[1]}/archive/refs/heads/main.zip`
-//byrizkyadi
-m.reply(`waiting for compress to zip`)
-conn.sendfile( m.chat, url, 'repo.zip', null, m)
+let url = `https://github.com/${args[0]}/${args[1]}/zipball/master`
+//ByRizkyAdi
+m.reply(`compressing data to zip file`)
+conn.sendFile( m.chat, url, 'repo.zip', null, m)
 
 }
-handler.help = ['githubdl']
-handler.tags = ['internet']
+handler.help = ['githubdl <username> <repo>']
+handler.tags = ['downloader']
 handler.command = /githubdl/i
 
-handler.limit = 5
+handler.limit = true
 
 module.exports = handler
