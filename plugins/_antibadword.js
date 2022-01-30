bf8fc7235b3c8af026adfbc0818b60e3c22deab0
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-30 21:03:44 +0700

    Update _antibadword.js

--- plugins/_antibadword.js
+++ plugins/_antibadword.js
@@ -5,25 +5,23 @@ let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?
 
 handler.before = function (m, { isOwner, isBotAdmin }) {
     if (m.isBaileys && m.fromMe) return !0
-    let chat = global.db.data.chats[m.chat]
-    let user = global.db.data.users[m.sender]
+    let chat = db.data.chats[m.chat]
+    let user = db.data.users[m.sender]
     let isBadword = badwordRegex.exec(m.text)
 
-    if (!chat.antiBadword && !chat.isBanned && isBadword) {
-        user.warn += 1
+    if (!chat.badword && !chat.isBanned && isBadword) {
+        user.warning += 1
         this.send2Button(m.chat, `*Badword terdeteksi!*
-Warning: ${user.warn} / 5
+Warning: ${user.warning} / 5
 Jika warning mencapai 5 kamu akan dibanned
-
 ketik *#on antibadword* untuk menyalakan antibadword
 ketik *#astagfirullah* atau *#maaf* untuk mengurangi warning
-
-“Barang siapa yang beriman kepada Allah dan Hari Akhir maka hendaklah dia berkata baik atau diam” (HR. al-Bukhari dan Muslim).`, footer, 'Nyalakan Antibadword', '.on antibadword', 'Astaghfirullah', '#maaf', m)
-        if (user.warn >= 5) {
+“Barang siapa yang beriman kepada Allah dan Hari Akhir maka hendaklah dia berkata baik atau diam” (HR. al-Bukhari dan Muslim).`, watermark, 'Nyalakan Antibadword', ',1 antibadword', 'Astaghfirullah', ',maaf', m)
+        if (user.warning >= 5) {
             user.banned = true
             if (m.isGroup) {
                 if (isBotAdmin) {
-                    this.groupSettingChange(m.chat, GroupSettingChange.messageSend, true)
+                    // this.groupSettingChange(m.chat, GroupSettingChange.messageSend, true)
                 }
             }
         }
