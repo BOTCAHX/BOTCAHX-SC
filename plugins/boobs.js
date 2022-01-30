6c301eac639deac9b0af4c36a6e84cfc3cdf4635
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-01-30 10:25:07 +0700

    Update boobs.js

--- plugins/boobs.js
+++ plugins/boobs.js
@@ -1,11 +1,11 @@
-let axios = require('axios')
+let axios = require('axios');
 let fetch = require('node-fetch')
 let neko = require('nekos.life')
 let Neko = new neko()
-     let handler  = async (m, { conn, usedPrefix, command }) => {
-     	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
-     json = await fetch(`https://meme-api.herokuapp.com/gimme/biganimetiddies`)
-   conn.sendButton(m.chat, await (await fetch(json.url)).buffer(), 'oppai', '© Asu', 'Next', `${usedPrefix + command}`, m, false)
+     let handler  = async (m, { conn, args }) => {
+if (!DATABASE.data.chats[m.chat].nsfw && m.isGroup) throw 'Feature Nsfw Disable\nType *!enable* *nsfw* to activate this feature'
+     json = (await axios.get('https://meme-api.herokuapp.com/gimme/biganimetiddies')).data
+   conn.sendFile(m.chat, json.url, 'boobs.jpg', '', m, false)
 }
 handler.help = ['boobs']
 handler.tags = ['hentai']
@@ -15,7 +15,6 @@ handler.mods = false
 handler.premium = false
 handler.group = false
 handler.private = false
-
 handler.limit = true
 handler.admin = false
 handler.botAdmin = false
