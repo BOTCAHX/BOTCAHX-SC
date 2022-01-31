bff2b1c0562ab91cdad06ae857e8bb4cd5df0b7c
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-02-01 02:34:55 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,11 +1,11 @@
 let fetch = require('node-fetch')
 let fs = require('fs')
 let handler = async(m, { conn, usedPrefix, text, command }) => {
-    if (!text) throw `Harap masukkan URL sebagai parameter.\n\nContoh: ${usedPrefix + command} https://vt.tiktok.com/xxxx`
-    let res = await https://ttdownloader.com/dl.php?v=YTo0OntzOjk6IndhdGVybWFyayI7YjowO3M6NzoidmlkZW9JZCI7czozMjoiM2RjYTY1ZjRiMTQxZDk5MzBhMjM4YjUxNjljMzQxZWIiO3M6MzoidWlkIjtzOjMyOiI4Mjc4NzVjYmM4NDgzZjcwMmMxZDc5NjM5YzgxNzExMSI7czo0OiJ0aW1lIjtpOjE2NDM2MjEwOTU7fQ=='))
+    if (!text) throw `Harap masukkan URL sebagai parameter.\n\nContoh: ${usedPrefix + command} https://vt.tiktok.com/ZSeSCAN1W/`
+    let res = await fetch(global.API('rey', '/api/download/tiktok', { url: text }, 'apikey'))
     if (!res.ok) throw await `${res.status} ${res.statusText}`
     let json = await res.json()
-    await conn.sendFile(m.chat, json.result.nowatermark, '', '©BOTCAHX', m)
+    await conn.sendFile(m.chat, json.result.nowatermark, '', 'Selesai...', m)
 }
 handler.command = /^tiktok$/i
 handler.tags = ['downloader']
