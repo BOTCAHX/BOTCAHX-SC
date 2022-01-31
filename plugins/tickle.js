0489cb95c035092433f3a88140e1556628f4c44a
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-02-01 06:45:15 +0700

    Create tickle.js

---
+++ plugins/tickle.js
@@ -0,0 +1,15 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command, usedPrefix }) => {
+	
+	conn.sendButtonImg(m.chat, await ( await fetch(`https://hardianto.xyz/api/anime/random?sfw=tickle&apikey=hardianto`)).buffer(), 'Nih kak', watermark, 'NEXT', `${usedPrefix + command}`, m)
+
+}
+
+handler.help = ['tickle']
+handler.tags = ['anime']
+handler.command = /^(tickle)$/i
+handler.register = true
+
+handler.limit = true
+
+module.exports = handler
