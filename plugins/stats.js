d3056a25ba99bdad1ce43087d1c5c9641bb541ee
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-02-04 06:03:26 +0700

    Update stats.js

--- plugins/stats.js
+++ plugins/stats.js
@@ -20,7 +20,7 @@ let handler  = async (m, { conn }) => {
 ❏ *Wa Web Name* : ${conn.browserDescription[0]}
 ❏ *Wa Web Version* : ${conn.browserDescription[2]}
 ❏ *Browser* : ${conn.browserDescription[1]}
-❏ *Platform* : Unbuntu Linux & RDP
+❏ *Platform* : Unbuntu Linux
 ❏ *Uptime Bot* : ${clockString(process.uptime() * 1000)}
 ❏ *Host Number* : @${global.conn.user.jid.split('@')[0]}
 ❏ *Bio Bot* : ${ppstatus.status}\n\n*Support/Follow Me*\nhttps://instagram.com/prm2.0?utm_medium=copy_link`, quoted: m, sendEphemeral: true, thumbnail: fs.readFileSync('./src/donasi.jpg'), contextInfo: { mentionedJid: [global.conn.user.jid]}})
