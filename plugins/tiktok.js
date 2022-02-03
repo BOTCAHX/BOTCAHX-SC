c5fe2dfd9afa13aa24a08b27b4f755bf8ef5666b
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-02-04 05:47:40 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let fs = require('fs')
 let handler = async(m, { conn, usedPrefix, text, command }) => {
-    if (!text) throw `Harap masukkan URL sebagai parameter.\n\nContoh: ${usedPrefix + command} https://vt.tiktok.com/ZSeSCAN1W/`
+    if (!text) throw `Harap masukkan URL sebagai parameter.\n\nContoh: ${usedPrefix + command} https://vm.tiktok.com/ZSepbsKQw/`
     let res = await fetch(global.API('rey', '/api/download/tiktok', { url: text }, 'apikey'))
     if (!res.ok) throw await `${res.status} ${res.statusText}`
     let json = await res.json()
