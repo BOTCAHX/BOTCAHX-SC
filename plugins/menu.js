72b5a61b6d73543dda8f6ac404ed2b015f692fc9
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-02-08 09:25:11 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -226,7 +226,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
       return conn.relayWAMessage(conn.prepareMessageFromContent(m.chat, {
         "listMessage": {
           "title": `${ucapan()}, ${name}`.trim(),
-          "description": `*©BOTCAHX*
+          "description": `*©ʙᴏᴛᴄᴀʜx*
          `.trim(),
           "footerText": "Jika menemukan bug,error atau kesulitan dalam penggunaan silahkan laporkan/tanyakan kepada owner.",
           "buttonText": "Click Here",
