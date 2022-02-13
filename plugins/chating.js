a7ea750d4975e1b1e54fca085cf5b065df993595
Tio <94074076+BOTCAHX@users.noreply.github.com>
2022-02-14 02:49:52 +0700

    Create chating.js

---
+++ plugins/chating.js
@@ -0,0 +1,23 @@
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+ye = `@${m.sender.split`@`[0]}`
+let info = `Hai Kak ${ye}
+`.trim()
+
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', `*${global.packname}*`, 'status@broadcast') 
+}
+handler.help = ['github']
+handler.tags = ['info']
+handler.command = ['script', 'sc', 'scbot', 'github']
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 25
+
+module.exports = handler
