4b7172a3f7e13f0b3f7b54f33cb9d20faac209f2
Tio <94074076+BOTCAHX@users.noreply.github.com>
2022-02-14 02:53:23 +0700

    Update chating.js

--- plugins/chating.js
+++ plugins/chating.js
@@ -1,15 +1,16 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 ye = `@${m.sender.split`@`[0]}`
 let info = `Hai Kak ${ye} Ini adalah website anonymous chat bot
+Cukup daftar nama lalu chating dengan orang random.
 
-https://anonymous-chat-random-tio.herokuapp.com/
+• https://anonymous-chat-random-tio.herokuapp.com/
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', `*${global.packname}*`, 'status@broadcast') 
 }
-handler.help = ['github']
-handler.tags = ['info']
-handler.command = ['script', 'sc', 'scbot', 'github']
+handler.help = ['anonimouschat']
+handler.tags = ['main']
+handler.command = ['anonymouschat']
 handler.owner = false
 handler.mods = false
 handler.premium = false
