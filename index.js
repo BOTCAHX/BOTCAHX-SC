e53316643c017c278b191e583853500637fd561b
Tio <94074076+BOTCAHX@users.noreply.github.com>
2022-02-22 04:52:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ CFonts.say('Rpg\nWhatsApp Bot', {
   align: 'center',
   gradient: ['red', 'magenta']
 })
-CFonts.say(`'${package.name}' By @${package.author.name || package.author}\nMikeBot ver 1.5.0\nRecode By Dawnfrost`, {
+CFonts.say(`'${package.name}' By @${package.author.name || package.author}\nBOTCAHX ver 1.5.0\nRecode By Tio`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
