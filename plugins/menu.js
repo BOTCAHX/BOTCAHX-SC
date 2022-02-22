6294d589b95e8657817225433f81047a99911d82
Tio <94074076+BOTCAHX@users.noreply.github.com>
2022-02-22 16:35:18 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -235,7 +235,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
           "listType": "SINGLE_SELECT",
           "sections": [
             {
-            "title": `"${ucapan()} : ${week} ${weton} : ${date}"`,
+            "title": `" ${ucapan()} : ${week} ${weton} : ${date} "`,
               "rows": [
                 {
                   "title": `|🧾| Semua Perintah`,
