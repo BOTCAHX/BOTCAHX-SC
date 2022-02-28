f540ef4428d660346a789c59db023881fe52489b
Tio <94074076+BOTCAHX@users.noreply.github.com>
2022-03-01 05:22:09 +0700

    Update kick.js

--- plugins/kick.js
+++ plugins/kick.js
@@ -1,9 +1,16 @@
 let handler = async (m, { conn, args }) => {
-  let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
-  let users = m.mentionedJid.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
+let fs = require('fs')
+ let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
+  aki = m.quoted ? [m.quoted.sender] : m.mentionedJid
+  let users = aki.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
+  wayy = '_*Dia sudah di tendang*_'
+  for (let i of users) {
+  wayy += ` @${i.split('@')[0]}`
+  }
+  conn.reply(m.chat, wayy, m, { contextInfo: { mentionedJid: users }})
   for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [user])
 }
-handler.help = ['kick', '-'].map(v => v + ' @user')
+handler.help = ['kick'].map(v => v + ' @user')
 handler.tags = ['admin']
 handler.command = /^(kick|\-)$/i
 handler.owner = false
@@ -16,7 +23,6 @@ handler.admin = true
 handler.botAdmin = true
 
 handler.fail = null
-handler.limit = 0
+handler.limit = false
 
 module.exports = handler
-
