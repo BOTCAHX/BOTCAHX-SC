be4f661798d8fdebe9ff0bc7e3caf85ee128429d
Tio <94074076+BOTCAHX@users.noreply.github.com>
2022-03-01 05:24:40 +0700

    Update add.js

--- plugins/add.js
+++ plugins/add.js
@@ -1,6 +1,5 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn, text, participants, usedPrefix, command }) => {
-  if (!text) throw `_Masukkan nomor!_ \nContoh:\n\n${usedPrefix + command + ' ' + global.owner[0]}`
+let handler = async (m, { conn, text, participants }) => {
   let _participants = participants.map(user => user.jid)
   let users = (await Promise.all(
     text.split(',')
@@ -12,7 +11,6 @@ let handler = async (m, { conn, text, participants, usedPrefix, command }) => {
       ])
   )).filter(v => v[1]).map(v => v[0] + '@c.us')
   let response = await conn.groupAdd(m.chat, users)
-  if (response[users] == 408) throw `Nomor tersebut telah keluar baru² ini\nHanya bisa masuk melalui ${usedPrefix}link`
   let pp = await conn.getProfilePicture(m.chat).catch(_ => false)
   let jpegThumbnail = pp ? await (await fetch(pp)).buffer() : false
   for (let user of response.participants.filter(user => Object.values(user)[0].code == 403)) {
@@ -44,7 +42,6 @@ handler.admin = true
 handler.botAdmin = true
 
 handler.fail = null
-handler.limit = 0
+handler.limit = true
 
 module.exports = handler
-
