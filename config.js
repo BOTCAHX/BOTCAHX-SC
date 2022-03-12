0fc3fb944c59c09aa927ad7f8293730f3cc5e592
Tio <94074076+BOTCAHX@users.noreply.github.com>
2022-03-12 09:29:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -51,7 +51,7 @@ global.packname = 'BOTCAHX' // ganti aja
 global.author = 'BOTCAHX' // ganti aja
 
 //yyy
-bc = 'Bot' //BOTCAHX BROADCAST
+bc = '' //BOTCAHX BROADCAST
 footer = '\n© BOTCAHX'
 namabot = '𝘽𝙊𝙏𝘾𝘼𝙃𝙓'
 namalu = 'Tio'
